725000566867e68ab0e64452120a28d9cb4b122c
ArunaKumariM <43091704+ArunaKumariM@users.noreply.github.com>
2018-11-27 18:53:56 +0530

    Add files via upload

---
+++ assignment8.1.r
@@ -0,0 +1,22 @@
+#Assignment 8.1
+# use  the package RmcdrPlugin .IPSUR
+# data(RmcdrTest) and perform the below Operations :
+
+install.packages("RmcdrPlugin.IPSUR")
+
+##############Getting package not found error#########
+
+
+
+
+
+
+library(RmcdrPlugin.IPSUR)
+#1 a. Calculate the average salary by gender and smoking status
+data(RmcdrTestDrive)
+
+#1 b. Which gender has the highest mean Salary
+#1.c. Report the highest mean salary
+#1.d. Compare the spreads for the genders by calculating the standard deviation
+#     of salary by gender
+# 
